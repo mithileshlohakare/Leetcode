8c20f090f3db319b0ece1ff80a2fcb48ecb6bbd5
Mithilesh Lohakare <mithileshlohakare2@gmail.com>
2026-06-22 20:25:56 +0530

    Time: 2 ms (96.67%), Space: 43.4 MB (62.01%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] count = new int[26];
+        for(char c: text.toCharArray()){
+            count[c - 'a']++;
+        }
+        int s = Math.min(
+            Math.min(count['b' - 'a'], count['a' - 'a']),
+            Math.min(count['l' - 'a'] / 2,
+            Math.min(count['o' - 'a'] / 2, count['n' - 'a']))
+        );
+        return s;
+    }
+}
