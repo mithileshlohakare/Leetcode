235974302bb6ff4cb84ca92c0fc0c3874fcb7497
Mithilesh Lohakare <mithileshlohakare2@gmail.com>
2026-07-01 23:06:08 +0530

    Time: 1536 ms (5.07%), Space: 142.3 MB (14.05%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,34 +1,27 @@
 class NumMatrix {
-
-	int[][] dp;
-
-	public NumMatrix(int[][] matrix) {
-		dp = new int[matrix.length][matrix[0].length];
-		populateArray(matrix, dp);
-	}
-
-	private void populateArray(int[][] arr, int[][] dp) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-				if (j == 0) {
-					dp[i][j] = arr[i][j];
-				} else {
-					dp[i][j] = arr[i][j] + dp[i][j - 1];
-				}
-			}
-		}
-	}
-
-	public int sumRegion(int row1, int col1, int row2, int col2) {
-		if (row2 >= dp.length || col2 >= dp[0].length || row1 < 0 || col1 < 0)
-			return -1;
-		int sum = 0;
-		for (int i = row1; i <= row2; i++) {
-			sum = sum + (dp[i][col2] - (col1 > 0 ? dp[i][col1 - 1] : 0));
-		}
-		return sum;
-	}
+    int[][] sum;
+    public NumMatrix(int[][] matrix) {
+        int m = matrix[0].length;
+        int n = matrix.length;
+        sum = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                sum[i][j] = matrix[i][j];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans = 0;
+        for(int i = row1; i < row2 + 1; i++){
+            for(int j = col1; j < col2 + 1; j++){
+                ans += sum[i][j];
+            }
+        }
+        return ans;
+    }
 }
+
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
