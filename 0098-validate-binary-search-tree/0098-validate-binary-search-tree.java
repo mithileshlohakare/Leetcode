15123781d66dbf306cca3744da4eeb0750f189c3
Mithilesh Lohakare <mithileshlohakare2@gmail.com>
2026-09-19 11:52:37 +0530

    Time: 0 ms (100%), Space: 44.7 MB (97.56%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,35 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+public class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode pre = null;
-        while(root != null || !st.isEmpty()){
-            while(root!=null){
-                st.push(root);
-                root = root.left;
-            }
-            root = st.pop();
-            if(pre != null && root.val <= pre.val){
-                return false;
-            }
-            pre = root;
-            root = root.right;
-        }
-        return true;
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
